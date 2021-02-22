cf01c41193cd8ea909631bcb0a127b9ece668682
StrongGarl1c <jarkkocs16@rambler.ru>
2021-02-22 15:26:24 +0200

    init: new game

---
+++ client/src/Barrel.js
@@ -0,0 +1,5 @@
+function Barrel({ position }) {
+  return <div style={position} className="barrel"></div>;
+}
+
+export default Barrel;
--- client/src/Map.js
+++ client/src/Map.js
@@ -1,118 +1,42 @@
 import { useState, useEffect } from 'react';
-import map from './885059-elwynn_forest.jpg';
 import './style.css';
-import ScoreBoard from './ScoreBoard';
+import Barrel from './Barrel';
 
 function Map() {
-  const [playerName, setPlayerName] = useState('Player 1');
-  const [clicks, setClicks] = useState(0);
-  const [distanceHint, setDistanceHint] = useState(
-    'Кликай по карте, а тут будет подсказка!',
-  );
-  const [target, setTarget] = useState({
-    x: getRandomNumber(500),
-    y: getRandomNumber(600),
-  });
-  const [gameIsFinished, setGameIsFinished] = useState(false);
+  const [position, setPosition] = useState([
+    {
+      top: Math.floor(Math.random() * 450),
+      left: Math.floor(Math.random() * 450),
+    },
+    {
+      top: Math.floor(Math.random() * 450),
+      left: Math.floor(Math.random() * 450),
+    },
+    {
+      top: Math.floor(Math.random() * 450),
+      left: Math.floor(Math.random() * 450),
+    },
+  ]);
+  const shuffle = () => {
+    setPosition(
+      position.map((item) => {
+        return {
+          top: Math.floor(Math.random() * (450 - item.top)),
+          left: Math.floor(Math.random() * (450 - item.left)),
+        };
+      }),
+    );
+  };
 
-  useEffect(() => {
-    let name = prompt('Введите ник');
-    if (name) {
-      setPlayerName(name);
-    }
-  }, []);
-
-  function getRandomNumber(size) {
-    return Math.floor(Math.random() * size);
-  }
-
-  // расчет расстояния от клика (event) до цели (target)
-  function getDistance(event, target) {
-    console.log('event', event.clientX, event.clientY);
-    console.log('target', target.x, target.y);
-    const diffX = event.clientX - target.x;
-    const diffY = event.clientY - target.y;
-    return Math.sqrt(diffX * diffX + diffY * diffY);
-  }
-
-  // строка подсказки расстояния
-  function getDistanceHint(distance) {
-    if (distance < 20) {
-      return 'Логово Рагнароса';
-    } else if (distance < 40) {
-      return 'Пригорает';
-    } else if (distance < 80) {
-      return 'Тепло';
-    } else if (distance < 160) {
-      return 'Прохладно';
-    } else if (distance < 320) {
-      return 'Очень холодно';
-    } else {
-      return 'Холодно как в нордсколе';
-    }
-  }
-
-  function clickHandler(event) {
-    setClicks(clicks + 1);
-    const distance = getDistance(event, target);
-    setDistanceHint(getDistanceHint(distance));
-    if (distance < 25) {
-      setGameIsFinished(true);
-      alert(
-        'Дробитель найден! Сделано кликов: ' +
-          clicks +
-          '\nНу ты индеец, БОМ, БОМ.',
-      );
-
-      // eslint-disable-next-line no-restricted-globals
-      if (confirm('Начать сначала?')) {
-        setClicks(0);
-        setDistanceHint('Кликай по карте, а тут будет подсказка!');
-        setTarget({
-          x: getRandomNumber(900),
-          y: getRandomNumber(600),
-        });
-        setGameIsFinished(false);
-      }
-    }
-
-    // fetch data
-    const data = {
-      name: playerName,
-      score: clicks,
-    };
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify(data),
-    };
-
-    // submit result
-    async function submitResult(response) {
-      response = await fetch('http://localhost:27017/submitResult', options);
-    }
-    submitResult().catch((err, wrn) => {
-      console.error(err);
-      console.warn(wrn);
-    });
-  }
   return (
-    <>
-      <div>
-        <h1>{playerName}, Найди Дробителя!</h1>
-        <p>{distanceHint}</p>
-        <img
-          onClick={gameIsFinished ? null : clickHandler}
-          src={map}
-          style={{ height: '600px' }}
-          alt="elwyn forest"
-        ></img>
+    <div>
+      <button onClick={shuffle}>shuffle</button>
+      <div id="map">
+        <Barrel position={position[0]} />
+        <Barrel position={position[1]} />
+        <Barrel position={position[2]} />
       </div>
-      <ScoreBoard />
-    </>
+    </div>
   );
 }
 
